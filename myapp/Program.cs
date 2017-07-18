1d1442e5b15aceec3474e8791c060de05af0a7b0
Xantiros <xantiros@gmail.com>
2017-07-18 15:32:23 +0200

    race

--- myapp/Program.cs
+++ myapp/Program.cs
@@ -7,9 +7,8 @@ namespace myapp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Order order1 = new Order(1,101);
-            User czlek = new User("cz@aabc.pl", "abc");
+            Race race = new Race();
+            race.Begin();
             
         }
     }
