dd9a65ea5b506a07130c1549d35a9a37c2ef9cdf
xantiros <xantiros@gmail.com>
2017-07-18 11:27:09 +0200

    commit

--- myapp/models/User.cs
+++ myapp/models/User.cs
@@ -11,6 +11,7 @@ namespace myapp.models
         public int Age { get; private set; }
         public bool IsActive { get; private set; }
         public DateTime UpdateDat { get; private set;}
+        public decimal Founds {get; private set;}
         public User(string email, string password)
         {
             SetEmail(email);
@@ -79,5 +80,26 @@ namespace myapp.models
             IsActive = false;
         }
         
+        public void PurchaseOrder(Order order)
+        {
+            if(!IsActive) throw new Exception("Only acrive user can purchase an order. ");
+
+            decimal orderPrice = order.TotalPrice;
+            if (Founds - orderPrice < 0)
+            {
+                throw new Exception("You don't have enough money.");
+            }
+            order.Purchase();
+            Founds -= orderPrice;
+            Update();
+        }
+
+        public void IncreaseFounds(decimal founds)
+        {
+            if (founds <= 0) throw new Exception("Founds mus be greater than 0");
+
+            Founds += founds;
+            Update();
+        }
     }
 }
