082c6065f00023e678f10093232fc5a71ddd0e5e
Xantiros <xantiros@gmail.com>
2017-07-19 11:16:08 +0200

    commit

--- myapp/Program.cs
+++ myapp/Program.cs
@@ -7,14 +7,8 @@ namespace myapp
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            Console.WriteLine("Hello World!");
-            Order order1 = new Order(1,105);
-            User czlek = new User("cz@aabc.pl", "abc");
-=======
             Race race1 = new Race();
             race1.Begin();
->>>>>>> lekcja2
             
         }
     }
